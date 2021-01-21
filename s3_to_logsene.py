fb1605a0b3829a2c45eea7fa31f406eded7f6ee0
Radu Gheorghe <radu.gheorghe@sematext.com>
2021-01-21 15:25:16 +0200

    updated code and readme to Python 3

--- s3_to_logsene.py
+++ s3_to_logsene.py
@@ -4,8 +4,12 @@ import boto3
 import socket
 import time
 import zlib
+import logging
 
-LOGSENE_SERVER = 'logsene-syslog-receiver.sematext.com'
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+LOGSENE_SERVER = 'logsene-receiver-syslog.sematext.com'
 LOGSENE_PORT = 514
 
 # application where you want to send your logs
@@ -19,37 +23,38 @@ logsene_dangling.close()
 
 def send_to_logsene(text, logsene_connection):
   to_send = "aws-lambda " + LOGSENE_APP_TOKEN + ":" + text + "\n"
-  logsene_connection.send(to_send)
-  #print "Sent string: " + to_send
+  logsene_connection.send(to_send.encode())
+  logger.debug("Sent string: " + to_send)
   
 
 def lambda_handler(event, context):
     logsene_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     logsene_connection.connect((LOGSENE_SERVER, LOGSENE_PORT))
     
-    print "Received event: " + json.dumps(event)
+    logger.debug("Received event: " + json.dumps(event))
 
     # Get the object from the event and show its content type
     bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key']).decode('utf8')
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
         body = response['Body']
         data = body.read()
     except Exception as e:
-        print 'Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket)
+        logger.error(f'Error getting object {key} from bucket {bucket}. Make sure they exist and your bucket is in the same region as this function.')
         logsene_connection.close()
         raise e
 
     try:
         data = zlib.decompress(data, 16+zlib.MAX_WBITS)
-        print "Detected gzipped content"
+        logger.debug("Detected gzipped content")
     except zlib.error:
-        print "Content couldn't be ungzipped, assuming plain text"
+        logger.error("Content couldn't be ungzipped, assuming plain text")
 
-    lines = data.split("\n")
+    lines = data.splitlines()
     
     for line in lines:
+        line = line.decode()
         try: # trying to parse JSON logs here
             json_line = json.loads(line)
             try:
@@ -58,8 +63,8 @@ def lambda_handler(event, context):
             except KeyError: # no CloudTrail logs then, will send the whole JSON line
                  send_to_logsene("@cee:" + line, logsene_connection)
         except ValueError: # ok, so it's not JSON
-            if line <> "": # let's skip empty strings
+            if line: # let's skip empty strings
                 send_to_logsene(line, logsene_connection)
     
     logsene_connection.close()
-    print "Done processing"
+    logger.debug("Done processing")
